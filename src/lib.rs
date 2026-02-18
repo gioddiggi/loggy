0435f5bdfab064335a32524ab8543dd25cade478
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2026-02-18 11:14:57 +0100

    chore: remove unused lib `time`

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,4 @@ pub mod logger;
 pub mod output;
 pub mod console;
 pub mod file;
-pub mod time;
 pub mod record;
--- src/time.rs
+++
@@ -1,5 +0,0 @@
-use chrono::Utc;
-
-pub fn current_timestamp() -> String {
-    Utc::now().to_rfc3339()
-}
